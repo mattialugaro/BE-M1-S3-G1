8015e2a4d25dfeb72593131a3335780729aab1c2
Mattia Lugaro <148619356+mattialugaro@users.noreply.github.com>
2024-02-12 15:00:10 +0100

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write(DateTime.Now);
+            Label1.Text = TextBox1.Text;
+            // test.InnerText = "Questo e' un testo";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox2.Text = ConfigurationManager.AppSettings["username"];
+        }
+    }
+}
